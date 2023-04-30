396ffab496a53ebd2f25c74f672327f7ede207d2
Aleksandr Mamlygo <alexander.mamlygo@gmail.com>
2023-04-30 16:43:43 +0400

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Aleksandr Mamlygo on 30.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
